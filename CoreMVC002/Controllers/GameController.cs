eb1e0feb256e726f13c69adc2067b33b973c2475
Freedom-Hank <hank3192@gmail.com>
2024-10-15 01:03:17 +0800

    更新遊戲引擎(新增猜測紀錄及猜測歷史)

--- CoreMVC002/Controllers/GameController.cs
+++ CoreMVC002/Controllers/GameController.cs
@@ -79,4 +79,3 @@ namespace CoreMVC002.Controllers
         }
     }
 }
-
--- CoreMVC002/Models/XAXBEngine.cs
+++ CoreMVC002/Models/XAXBEngine.cs
@@ -5,6 +5,12 @@
         public string Secret { get; set; }
         public string Guess { get; set; }
         public string Result { get; set; }
+        // 猜測的歷史記錄，每次猜測的內容與結果
+        public List<string> GuessHistory { get; set; } = new List<string>();
+        // 猜測次數的累計
+        public int GuessCount { get; set; }
+        // 猜對後的遊戲結束提示信息
+        public string GameOverMessage { get; set; }
 
         public XAXBEngine()
         {
@@ -14,6 +20,8 @@
             Secret = randomSecret;
             Guess = null;
             Result = null;
+            GuessCount = 0;
+            GameOverMessage = null;
         }
 
         public XAXBEngine(string secretNumber)
@@ -21,6 +29,8 @@
             Secret = secretNumber;
             Guess = null;
             Result = null;
+            GuessCount = 0;
+            GameOverMessage = null;
         }
         //
         public int numOfA(string guessNumber)
@@ -38,9 +48,20 @@
         public bool IsGameOver(string guessNumber)
         {
             // TODO 3
+            if (Secret.Equals(guessNumber))
+            {
+                GameOverMessage = "恭喜你猜對了！是否重新開始遊戲？";
+                return true;
+            }
             return false;
         }
-
+        public void AddGuessHistory(string guess, string result)
+        {
+            // 將本次猜測內容和結果添加到歷史記錄中
+            GuessHistory.Add($"猜測: {guess}, 結果: {result}");
+            // 增加猜測次數
+            GuessCount++;
+        }
     }
 
 }
